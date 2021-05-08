31465030279c8b187d6052b6aa1790fd90d2cf3b
Syd Amir <hemedani@gmail.com>
2021-05-08 10:56:18 +0430

    🎉  First fixed of idea

---
+++ src/mod.ts
@@ -0,0 +1,58 @@
+import FastestValidator from "https://esm.sh/fastest-validator@1";
+import { exists } from "https://deno.land/std/fs/mod.ts";
+import { throwError } from "./throwError.ts";
+
+export const create = <T>(
+  init: T,
+  schema: Record<string, unknown>,
+  path: string,
+) => {
+  let db = init;
+
+  const v = new FastestValidator();
+  const check = v.compile(schema);
+
+  const setup = async () => {
+    const createFile = async (data: string) => {
+      await Deno.writeTextFile(path, data);
+    };
+
+    const setOnState = async () => {
+      const readState = JSON.parse(
+        await Deno.readTextFile(path),
+      );
+
+      return (db = {
+        ...readState,
+      });
+    };
+
+    (await exists(path))
+      ? await setOnState()
+      : await createFile(JSON.stringify(db));
+  };
+
+  const checkData = (data: T) => {
+    const isRight = check(data);
+    return isRight === true
+      ? isRight
+      : throwError(`${isRight[0].message} but get ${isRight[0].actual}`);
+  };
+
+  const set = async (data: T) => {
+    db = {
+      ...data,
+    };
+    await Deno.writeTextFile(
+      path,
+      JSON.stringify(data),
+    );
+  };
+
+  return {
+    setup,
+    set,
+    checkData,
+    db,
+  };
+};
